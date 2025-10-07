112700d5683238aa2a296151dbe964f498c88849
ChengWan9 <20377398@buaa.edu.cn>
2025-10-07 11:35:16 +0000

    # CME: final touches on CME. Removed redundant code.

--- CME/CME_CPU.py
+++ CME/CME_CPU.py
@@ -2,66 +2,6 @@ import numpy as np
 from numba import njit, prange
 import typing
 
-correction_config = {
-    "iterations"    : 50,
-    "tp_cutoff"     : 0.95,
-    "fp_cutoff"     : 0.05,
-    "fp_val"        : 0,
-    "tp_val"        : None,
-    "phony_val"  : 0.2
-}
-
-cme_config = {
-    "device"            : "CPU",
-    "normalize"         : True,
-    "threads_per_block"   : 256
-}
-
-# input X has to be a numpy array of float32.
-# Rows of X has to be genes. Column cells.
-def CME_corrected(X: np.ndarray[tuple[int, int], np.dtype[np.float32]],
-                    feature_indices: np.ndarray[tuple[int], np.dtype[np.int32]] = None):
-    # Get the configs
-    iterations = correction_config["iterations"]
-    tp_cutoff = correction_config["tp_cutoff"]
-    fp_cutoff = correction_config["fp_cutoff"]
-    tp_val = correction_config["tp_val"]
-    fp_val = correction_config["fp_val"]
-    phony_val = correction_config["phony_val"]
-    normalize = cme_config["normalize"]
-
-    cme = CME_cpu(X, normalize, feature_indices)
-    null_cme_ary = rand_permute_CME(X, iterations, normalize, feature_indices)
-    print(null_cme_ary)
-    pval_mtx = CME_pva_cpu(cme, null_cme_ary)
-    print(pval_mtx)
-    cme_corrected = CME_correction_by_pval(cme, pval_mtx, tp_cutoff, fp_cutoff, tp_val, fp_val, phony_val)
-
-    return cme_corrected, pval_mtx
-
-
-def CME_correction_by_pval(cme: np.ndarray[tuple[int, int], np.dtype[np.float32]],
-                            pval_mtx: np.ndarray[tuple[int, int], np.dtype[np.float32]],
-                            tp_cutoff: float = 0.95, fp_cutoff: float = 0.05,
-                            tp_val: typing.Optional[float] = None,
-                            fp_val: float = 0,
-                            phony_val: typing.Optional[float] = None
-                            ) -> np.ndarray[tuple[int], np.dtype[np.float32]]:
-    cme_corrected = cme.copy()
-    
-    # Correct the FP CME values
-    cme_corrected[pval_mtx < fp_cutoff] = fp_val
-    # Correct the TP CME values
-    if tp_val is not None:
-        cme_corrected[pval_mtx > tp_cutoff] = tp_val
-    
-    # Correct the unrelated CME values
-    if phony_val is not None:
-        cme_corrected[(pval_mtx >= fp_cutoff) &
-                  (pval_mtx <= tp_cutoff)] = phony_val
-    
-    return cme_corrected
-
 
 def CME_pva_cpu(cme: np.ndarray[tuple[int, int], np.dtype[np.float32]],
                     null_cme_ary: np.ndarray[tuple[int, int, int], np.dtype[np.float32]]
@@ -71,23 +11,6 @@ def CME_pva_cpu(cme: np.ndarray[tuple[int, int], np.dtype[np.float32]],
     pval_mtx = count_matrix / float(null_cme_ary.shape[0])
 
     return pval_mtx
-
-
-def rand_permute_CME(X: np.ndarray[tuple[int, int], np.dtype[np.float32]],
-                        iterations: int = 50, normalize: bool = True,
-                        feature_indices: np.ndarray[int, np.dtype[np.int32]] = None
-                        ) -> np.ndarray[tuple[int, int, int], np.dtype[np.float32]]:
-    null_cme_ary = []
-
-    for i in range(iterations):
-
-        X_shuffled = shuffle_and_normalize_cpu(X)
-
-        cme_shuffled = CME_cpu(X_shuffled, normalize=normalize, feature_indices=feature_indices)
-        null_cme_ary.append(cme_shuffled)
-
-    null_cme_ary = np.array(null_cme_ary)
-    return null_cme_ary
 
 
 def shuffle_and_normalize_cpu(X: np.ndarray[tuple[int, int], np.dtype[np.float32]]
